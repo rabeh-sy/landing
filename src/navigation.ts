68177db8b6be8c237d9d07f767c5ec255c446e99
khaled-badenjki <m.k.badenjki2@gmail.com>
2025-07-14 07:32:11 +0300

    update content

--- src/navigation.ts
+++ src/navigation.ts
@@ -15,7 +15,7 @@ export const headerData = {
       href: getBlogPermalink(),
     },
   ],
-  actions: [{ text: 'ابدأ الآن', href: 'https://wa.me/+963997325331?text=%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%20%D8%B9%D9%84%D9%8A%D9%83%D9%85%D8%8C%20%D8%A3%D9%88%D8%AF%20%D9%85%D8%B9%D8%B1%D9%81%D8%A9%20%D8%A7%D9%84%D9%85%D8%B2%D9%8A%D8%AF%20%D8%B9%D9%86%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%D9%83%D9%85', target: '_blank', icon: 'tabler:brand-whatsapp' }],
+  actions: [{ text: 'يالله نتكلّم', href: 'https://wa.me/+963997325331?text=%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%20%D8%B9%D9%84%D9%8A%D9%83%D9%85%D8%8C%20%D8%A3%D9%88%D8%AF%20%D9%85%D8%B9%D8%B1%D9%81%D8%A9%20%D8%A7%D9%84%D9%85%D8%B2%D9%8A%D8%AF%20%D8%B9%D9%86%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%D9%83%D9%85', target: '_blank', icon: 'tabler:brand-whatsapp' }],
 };
 
 export const footerData = {
@@ -40,6 +40,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    رابح شركة تكنولوجية سورية متخصصة في التحول الرقمي
+    رابح للتقنية شركة سورية متخصصة في التحول الرقمي
   `,
 };
