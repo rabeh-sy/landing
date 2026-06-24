89b86843973a970f97ed3cce887e44eba971fd0b
khaled-badenjki <m.k.badenjki2@gmail.com>
2026-06-24 23:37:46 +0300

    badenjki business

--- src/navigation.ts
+++ src/navigation.ts
@@ -20,15 +20,22 @@ export const headerData = {
       href: getPermalink('/contact'),
     },
     {
-      text: 'رابح لايت',
-      href: getPermalink('/rabeh-lite'),
+      text: 'بادنجكي الإداري',
+      href: getPermalink('/badenjki-business'),
     },
     {
       text: 'المدونة',
       href: getBlogPermalink(),
     },
   ],
-  actions: [{ text: 'الاستشارة مجانية - تواصل معنا', href: 'https://wa.me/+963954183399?text=%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%20%D8%B9%D9%84%D9%8A%D9%83%D9%85%D8%8C%20%D8%A3%D9%88%D8%AF%20%D9%85%D8%B9%D8%B1%D9%81%D8%A9%20%D8%A7%D9%84%D9%85%D8%B2%D9%8A%D8%AF%20%D8%B9%D9%86%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%D9%83%D9%85', target: '_blank', icon: 'tabler:brand-whatsapp' }],
+  actions: [
+    {
+      text: 'الاستشارة مجانية - تواصل معنا',
+      href: 'https://wa.me/+963954183399?text=%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%20%D8%B9%D9%84%D9%8A%D9%83%D9%85%D8%8C%20%D8%A3%D9%88%D8%AF%20%D9%85%D8%B9%D8%B1%D9%81%D8%A9%20%D8%A7%D9%84%D9%85%D8%B2%D9%8A%D8%AF%20%D8%B9%D9%86%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%D9%83%D9%85',
+      target: '_blank',
+      icon: 'tabler:brand-whatsapp',
+    },
+  ],
 };
 
 export const footerData = {
@@ -39,11 +46,12 @@ export const footerData = {
         { text: 'اتصل بنا', href: getPermalink('/contact') },
         { text: 'المدونة', href: getBlogPermalink() },
       ],
-    }
+    },
   ],
   secondaryLinks: [
     { text: 'الشروط', href: getPermalink('/terms') },
     { text: 'سياسة الخصوصية', href: getPermalink('/privacy') },
+    { text: 'خصوصية بادنجكي الإداري', href: getPermalink('/badenjki-business/privacy') },
   ],
   socialLinks: [
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/rabeh.tech.sy' },
