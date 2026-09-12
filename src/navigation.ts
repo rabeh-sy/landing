242c0807a7e11fc0e34e64771e49ca9e561ce92b
khaled-badenjki <m.k.badenjki2@gmail.com>
2026-09-12 15:51:52 +0300

    major update

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,34 +3,25 @@ import { getPermalink, getBlogPermalink } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'الرئيسية',
+      text: 'الاستوديو',
       href: getPermalink('/'),
-    },
-    // {
-    //   text: 'خدماتنا',
-    //   links: [
-    //     {
-    //       text: 'بناء البرامج على حسب الطلب',
-    //       href: getPermalink('/services/custom-software-development'),
-    //     },
-    //   ]
-    // },
-    {
-      text: 'اتصل بنا',
-      href: getPermalink('/contact'),
     },
     {
       text: 'بادنجكي الإداري',
       href: getPermalink('/badenjki-business'),
     },
     {
-      text: 'المدونة',
+      text: 'المقالات',
       href: getBlogPermalink(),
+    },
+    {
+      text: 'تواصل',
+      href: getPermalink('/contact'),
     },
   ],
   actions: [
     {
-      text: 'الاستشارة مجانية - تواصل معنا',
+      text: 'ابدأ مشروعًا',
       href: 'https://wa.me/+963954183399?text=%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%20%D8%B9%D9%84%D9%8A%D9%83%D9%85%D8%8C%20%D8%A3%D9%88%D8%AF%20%D9%85%D8%B9%D8%B1%D9%81%D8%A9%20%D8%A7%D9%84%D9%85%D8%B2%D9%8A%D8%AF%20%D8%B9%D9%86%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%D9%83%D9%85',
       target: '_blank',
       icon: 'tabler:brand-whatsapp',
@@ -41,10 +32,11 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'روابط ذات صلة',
+      title: 'استكشف',
       links: [
-        { text: 'اتصل بنا', href: getPermalink('/contact') },
-        { text: 'المدونة', href: getBlogPermalink() },
+        { text: 'بادنجكي الإداري', href: getPermalink('/badenjki-business') },
+        { text: 'المقالات', href: getBlogPermalink() },
+        { text: 'تواصل', href: getPermalink('/contact') },
       ],
     },
   ],
